37fc5466f42bcba9b7fe182de5ebc2e52262ee1e
Saju Paul <saju.christel@blu-pa.com>
2019-08-10 18:18:37 +0530

    Adding repository and spring data JPA

---
+++ PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/data/UserRepository.java
@@ -0,0 +1,6 @@
+package com.appdeveloperblog.photoapp.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}

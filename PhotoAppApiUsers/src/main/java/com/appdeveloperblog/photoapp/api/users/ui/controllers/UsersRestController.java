182fcec90d31a5f9fd435ccb57f00e85b4007841
Saju Paul <saju.christel@blu-pa.com>
2019-08-10 22:10:04 +0530

    Adding mediatype xml

--- PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
+++ PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
@@ -9,6 +9,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,7 +33,8 @@ public class UsersRestController {
         return "Working port:" + environment.getProperty("local.server.port");
     }
 
-    @PostMapping()
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel createUserRequestModel) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);

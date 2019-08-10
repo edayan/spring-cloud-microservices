64e23bdf848b5b5671c1a1dc136ff52e958b6b76
Saju Paul <saju.christel@blu-pa.com>
2019-08-10 14:26:54 +0530

    configure zuul api for gateway api

--- PhotoAppApiZuulApiGateway/src/main/java/com/appdeveloperblog/photoapp/api/apigateway/PhotoAppApiZuulApiGatewayApplication.java
+++ PhotoAppApiZuulApiGateway/src/main/java/com/appdeveloperblog/photoapp/api/apigateway/PhotoAppApiZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.appdeveloperblog.photoapp.api.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class PhotoAppApiZuulApiGatewayApplication {
 
 	public static void main(String[] args) {

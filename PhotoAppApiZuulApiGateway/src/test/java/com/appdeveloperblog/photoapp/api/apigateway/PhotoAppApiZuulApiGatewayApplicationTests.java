7960d8ad7c0adbb05ce01c19596827b16eb2bf70
Saju Paul <saju.christel@blu-pa.com>
2019-08-10 14:01:52 +0530

    Zuul Api gateway initial commit

---
+++ PhotoAppApiZuulApiGateway/src/test/java/com/appdeveloperblog/photoapp/api/apigateway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appdeveloperblog.photoapp.api.apigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

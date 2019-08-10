ddf5e7f150799e2b0face8840b88bf6461cc54bc
Saju Paul <saju.christel@blu-pa.com>
2019-08-10 14:42:18 +0530

    running multiple instance of users api

--- PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
+++ PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
@@ -1,5 +1,7 @@
 package com.appdeveloperblog.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersRestController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
-    public static String status() {
-        return "Working";
+    public  String status() {
+        return "Working port:" + environment.getProperty("local.server.port");
     }
 }

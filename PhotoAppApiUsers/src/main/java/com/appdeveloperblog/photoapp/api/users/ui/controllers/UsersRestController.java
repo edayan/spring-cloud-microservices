95dd1d6beba62c690f0e01553d4ae8daa92b38d5
Saju Paul <saju.christel@blu-pa.com>
2019-08-20 22:00:16 +0530

    Change for testing bus-refresh

--- PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
+++ PhotoAppApiUsers/src/main/java/com/appdeveloperblog/photoapp/api/users/ui/controllers/UsersRestController.java
@@ -30,7 +30,8 @@ public class UsersRestController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working port:" + environment.getProperty("local.server.port");
+        return "Working port:" + environment.getProperty("local.server.port")
+                +", with token="+ environment.getProperty("token.secret");
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},

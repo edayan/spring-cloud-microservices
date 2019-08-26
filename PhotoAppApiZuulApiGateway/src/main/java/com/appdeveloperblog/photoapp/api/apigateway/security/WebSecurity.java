261bc4034b5d9c01285982c86da3ce9bf3aebbdc
Saju Paul <saju.christel@blu-pa.com>
2019-08-26 20:03:51 +0530

    enable actuator for zuul api

--- PhotoAppApiZuulApiGateway/src/main/java/com/appdeveloperblog/photoapp/api/apigateway/security/WebSecurity.java
+++ PhotoAppApiZuulApiGateway/src/main/java/com/appdeveloperblog/photoapp/api/apigateway/security/WebSecurity.java
@@ -26,6 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
         httpSecurity.authorizeRequests()
+                .antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
                 .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
